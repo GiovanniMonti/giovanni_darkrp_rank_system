4f705c667ab855410463a4135998dc6a7892f05e
Giovanni Monti <smartdavide@gmail.com>
2021-12-10 22:44:42 +0100

    hud work

--- lua/darkrp_ranks/cl_drpranks.lua
+++ lua/darkrp_ranks/cl_drpranks.lua
@@ -13,7 +13,7 @@ net.Receive("OpenJRSMenu", function() JRS:OpenMenu() end)
 
 function JRS:OpenMenu()
 
-    local w =  ScrW()/5
+    local w =  ScrW()/4
     local h =  ScrH()/2
 
     local FrameColor = Color(50, 50, 50, 245)
@@ -31,22 +31,49 @@ function JRS:OpenMenu()
     
     Frame:MakePopup()
 
-    function Frame:Paint(w,h)
-        draw.RoundedBox(8, 0, 0, w, h, FrameColor)
+    function Frame:Paint(wi,hi)
+        draw.RoundedBox(8, 0, 0, wi, hi, FrameColor)
     end
 
-    local PlayerSearch = vgui.Create("DTextEntry", Frame)
 
-    PlayerSearch:SetSize( w * 0.85, h * 0.035 )
+    local PlyList = vgui.Create("DListView", Frame)
+
+    PlyList:Dock(NODOCK)
+    PlyList:SetPos( w*0.04, h*0.1 )
+    PlyList:SetSize( w * 0.9, h * 0.5 )
+    PlyList:SetMultiSelect( false )
+
+    PlyList:AddColumn("Name")
+    PlyList:AddColumn("SteamID")
+    PlyList:AddColumn("SteamID64")
+        
+    for _,v in pairs( player.GetHumans() ) do
+        PlyList:AddLine( v:Nick(), v:SteamID(), v:SteamID64() )
+    end
+
+
+    function PlyList:OnRowRightClick(lineID, line)
+
+    end
+    
+    local PlayerSearch = vgui.Create("DTextEntry", Frame)
+    local PlySearchText = ""
+
+    PlayerSearch:SetSize( w * 0.9, h * 0.035 )
     PlayerSearch:SetPos( w*0.04, h*0.05 )
     PlayerSearch:SetText( "Search for a Player (Nick/SteamID/SteamID64)" )
+    function PlayerSearch:OnGetFocus()
+        PlayerSearch:SetText( "" )
+    end
     function PlayerSearch:OnEnter()
-        local Text = self:GetValue()
-        Text = IsPlyNick(Text)
-        if Text then
-            --- make search & list
-        else
-            -- error messege
+        PlySearchText = self:GetValue()
+        PlyList:Clear()
+
+        for _,v in pairs( player.GetHumans() ) do
+            if ( string.find( string.lower( v:Nick() ), string.lower( PlySearchText ) ) or string.find(v:SteamID64(), PlySearchText) or string.find(v:SteamID(), PlySearchText) ) then
+                PlyList:AddLine( v:Nick(), v:SteamID(), v:SteamID64() )
+            end
         end
     end
+    
 end

7115b75d2c03817cbbf25dfba67220a92d36e8b0
Giovanni Monti <smartdavide@gmail.com>
2021-12-19 16:51:37 +0100

    dcomboboxes on GUI are mostly done.

--- lua/autorun/sh_darkrpranks_init.lua
+++ lua/autorun/sh_darkrpranks_init.lua
@@ -1,7 +1,15 @@
 JRS = JRS or {}
 
-AddCSLuaFile("darkrp_ranks/sh_cami.lua")
+if SERVER then 
+    AddCSLuaFile("darkrp_ranks/sh_cami.lua") 
+    AddCSLuaFile("darkrp_ranks/sh_drpranks.lua")
+    AddCSLuaFile("darkrp_ranks/sh_drpranks_cfg.lua")
+    AddCSLuaFile("darkrp_ranks/cl_drpranks.lua")
+
+end
+
 include("darkrp_ranks/sh_cami.lua")
+include("darkrp_ranks/sh_drpranks.lua")
 
 if SERVER then
 
@@ -9,19 +17,12 @@ if SERVER then
     print("---- Giovanni's Rank System ----")
     print("------ Loaded version 1.0 ------")
     print("--------------------------------")
-
-    AddCSLuaFile("darkrp_ranks/sh_drpranks.lua")
-    AddCSLuaFile("darkrp_ranks/sh_drpranks_cfg.lua")
-    AddCSLuaFile("darkrp_ranks/cl_drpranks.lua")
-    AddCSLuaFile("darkrp_ranks/sh_cami.lua")
-
-    include("darkrp_ranks/sh_drpranks.lua")
+    
     include("darkrp_ranks/sv_drpranks.lua")
 end
 
 include("darkrp_ranks/sh_drpranks_cfg.lua")
 
 if CLIENT then
-    include("darkrp_ranks/sh_drpranks.lua")
     include("darkrp_ranks/cl_drpranks.lua")
 end
